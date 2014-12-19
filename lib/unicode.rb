cd85a8bf7d15152551cf5d50584c4d5b4feb7aa5
Jens Wille <jens.wille@gmail.com>
2014-12-19 13:52:43 +0100

    lib/unicode.rb: Ruby 1.9.3 compatibility. (230075c)

--- lib/unicode.rb
+++ lib/unicode.rb
@@ -1,6 +1,6 @@
 begin
   require "unicode/#{RUBY_VERSION[/\d+.\d+/]}/unicode_native"
 rescue LoadError => err
-  raise unless err.path
+  raise if err.respond_to?(:path) && !err.path
   require 'unicode/unicode_native'
 end
