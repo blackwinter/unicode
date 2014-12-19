230075c4c563f0bda16e0c6f8b54916926fadb4f
Jens Wille <jens.wille@gmail.com>
2014-12-19 12:29:41 +0100

    lib/unicode.rb: Reraise extension LoadError other than "cannot load such file".

--- lib/unicode.rb
+++ lib/unicode.rb
@@ -1,6 +1,6 @@
 begin
-  RUBY_VERSION =~ /(\d+.\d+)/
-  require "unicode/#{$1}/unicode_native"
-rescue LoadError
+  require "unicode/#{RUBY_VERSION[/\d+.\d+/]}/unicode_native"
+rescue LoadError => err
+  raise unless err.path
   require 'unicode/unicode_native'
 end
